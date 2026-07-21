c17ddb8c5daac215a2a653b232648b94feccb091
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-07-21 23:11:19 +0500

    Loops, Switch Statements

--- script.js
+++ script.js
@@ -2,14 +2,32 @@
 
  document.write("<h1>" + "Question 1" + "</h1>" );
 
-  function power( a, b){
+
+  function power(a,b){
     let result = 1;
     let i = 1;
-    while(i <= b){
-        result = result *a;
-        i++;
+    while( i <= b){
+     result = result * a;
+     i++;
     }
     return result;
   }
-  let answer = power(2,5);
+  let answer = power(2,3);
   document.write(answer);
+
+//   Question 2 
+
+  document.write("<h1>" + "Question 2" + "</h1>" );
+
+
+  function leap(year){
+  if (year %4 == 0){
+   return "The year is leap year";
+  }
+  else{
+     return "The year is  not leap year";
+  }
+
+  }
+ let inputyear = +prompt("Enter any year");
+   document.write(leap(inputyear));
